0e5f6be388f3736f3bc9a2125612f37e95a169b0
caiosanvi <caiosanvi@hotmail.com>
2020-07-27 17:56:05 -0300

    criando funcoes

---
+++ funcao.js
@@ -0,0 +1,23 @@
+function soma(a,b){
+    return a + b;
+};
+
+
+function subtracao(a,b){
+    return a - b;
+}
+
+
+function divisao(a,b){
+    return a / b;
+}
+
+
+function multiplicacao(a,b){
+    return a * b;
+}
+
+console.log(soma(10,10)) 
+console.log(subtracao(30,10))
+console.log(divisao(10,2))
+console.log(multiplicacao(5,2))
